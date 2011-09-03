786a0ce1fc30e97ba749f91a9fde666c2d8b060c
Cameron Dykes <yellow5c@gmail.com>
2011-09-03 18:24:12 -0500

    Configure the app to compile styl files to css

--- web.js
+++ web.js
@@ -3,6 +3,23 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.register('.haml', require('hamljs'));
 
+app.configure(function ()
+{
+    this.set("views", __dirname + "/views");
+    this.set("view engine", "hamljs");
+
+    this.use(express.bodyParser());
+    this.use(express.methodOverride());
+
+    this.use(require("stylus").middleware({
+        src: __dirname + "/views",
+        compress: true
+    }));
+
+    this.use(express.static(__dirname + '/views'));
+    this.use(this.router);
+});
+
 app.get('/', function(request, response) {
   response.render('index.haml', { layout: false });
 });
