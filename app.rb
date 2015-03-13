54b8be184527fcbcf163c1d1b601ee883411e93c
Cameron Dykes <yellow5c@gmail.com>
2015-03-13 02:36:52 +0000

    Reconfigure Cuba to render the haml template

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'cuba'
+require 'cuba/render'
+require 'tilt/haml'
+
+# Configure Cuba to render haml templates
+Cuba.plugin Cuba::Render
+Cuba.settings[:render][:template_engine] = 'haml'
 
 Cuba.define do
+  # Render the index page wrapped in the layout.
   on get, root do
-    res.write('yellow5 is alive!')
+    render('index')
   end
 end
