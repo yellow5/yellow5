a2f2a4533ccea76cee4a44d13a3ec60daa5c8cf0
Cameron Dykes <yellow5c@gmail.com>
2015-03-13 03:14:10 +0000

    Update Cuba to render static files from public

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'cuba'
 require 'cuba/render'
 require 'tilt/haml'
 
+# Configure Cuba to serve static files
+Cuba.use Rack::Static, root: 'public', urls: [ '/images', '/styles' ]
+
 # Configure Cuba to render haml templates
 Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = 'haml'
