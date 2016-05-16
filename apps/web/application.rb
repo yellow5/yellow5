c69dd659f7100eec402998b03807062db423d0ac
Cameron Dykes <yellow5c@gmail.com>
2016-05-16 15:36:11 -0500

    Disable asset compilation in development

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -231,6 +231,10 @@ module Web
     configure :development do
       # Don't handle exceptions, render the stack trace
       handle_exceptions false
+
+      assets do
+        compile false
+      end
     end
 
     ##
