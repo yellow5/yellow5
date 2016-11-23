09c56631e90d49c77fd91ec82a5ec8935a703ced
Cameron Dykes <yellow5c@gmail.com>
2016-11-23 11:46:46 -0600

    Refactor main page to render a 5

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Yellow5.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "<div id=\"yellow5\">5</div>"
   end
 end
