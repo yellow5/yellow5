b41977f57c2f0f6fcabfc2d68b9dea422bfce66a
Cameron Dykes <yellow5c@gmail.com>
2016-11-24 20:50:17 -0600

    Umm, get_env not gen_env silly!

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :yellow5, Yellow5.Endpoint,
   url: [scheme: "https", host: "thawing-river-14713.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forward_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.gen_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
