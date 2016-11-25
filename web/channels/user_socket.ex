379dc7585ba31f762f51f77c8029ef24c1ff5fdf
Cameron Dykes <yellow5c@gmail.com>
2016-11-24 20:47:20 -0600

    Add channel timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Yellow5.UserSocket do
   # channel "room:*", Yellow5.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
